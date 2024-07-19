be20dc7be924285c3b92270dc6ddcb9ccf83115e
tientscode <tienit.tscode@gmail.com>
2024-07-19 19:29:36 +0700

    YTB

---
+++ src/main/java/com/tscocde/YTB/YtbApplication.java
@@ -0,0 +1,13 @@
+package com.tscocde.YTB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtbApplication.class, args);
+	}
+
+}
